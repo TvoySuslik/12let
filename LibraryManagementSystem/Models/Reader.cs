e2c05bc2a306501c467af9dba465ca71d4683dc4
TvoySuslik <04kirill03kn@gmail.com>
2024-11-21 09:39:22 +0300

    task2

---
+++ LibraryManagementSystem/Models/Reader.cs
@@ -0,0 +1,14 @@
+﻿namespace LibraryManagementSystem.Models
+{
+    public class Reader
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override string ToString()
+        {
+            return $"[ID: {Id}] {Name}, Email: {Email}";
+        }
+    }
+}
